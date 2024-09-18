9dc6732e130e9362fbf9ef529b0e33bbec0704df
catherineimes <cimes01@gmail.com>
2024-09-18 10:57:19 -0500

    First draft of template for calling the docupanda API

--- main.py
+++ main.py
@@ -4,12 +4,11 @@
 
 from utils.apihandler import ApiHandler
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     api_handler = ApiHandler()
-    #api_handler.post_document(pass your document here)
+    #api_handler.post_document(pass the path to your document here)
 
-    print(api_handler.get_standardized_output())
+    print(api_handler.get_standardized_output().text)
+    print(api_handler.get_full_ouptut("fb838180").text)
 
 
--- utils/apihandler.py
+++ utils/apihandler.py
@@ -5,8 +5,6 @@ class ApiHandler:
     def __init__(self):
        self.api_key = const.DOCUPANDA_API_KEY
 
-
-
     def post_document(self, fileName):
         doc_url = f"{const.DOCUPANDA_DOC_URL}{fileName}"
         payload = {"document": {"file": {
@@ -25,8 +23,20 @@ class ApiHandler:
 
         return document_id
 
+    def get_full_ouptut(self, document_id):
+        url = f"{const.DOCUPANDA_DOC_URL}{document_id}"
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-Key": self.api_key
+        }
+        response = requests.get(url,headers=headers)
+        return response
+
+
     def get_standardized_output(self):
-        std_url = const.DOCUPANDA_STD_URL
+        std_url = f"{const.DOCUPANDA_STD_URL}"
+        print(std_url)
         headers = {"accept": "application/json",
                    "X-API-KEY": self.api_key}
 
