4e35fc045c2921b225a7d48127b33a89bdb37479
catherineimes <cimes01@gmail.com>
2024-10-10 10:21:39 -0500

    Made minor updates to correct issue with post_document, created 1 method to process all pages, and another that will take a list of pages.

--- utils/apihandler.py
+++ utils/apihandler.py
@@ -4,25 +4,46 @@ import requests
 class ApiHandler:
     def __init__(self):
        self.api_key = const.DOCUPANDA_API_KEY
+       print(type(self.api_key))
 
-    def post_document(self, fileName):
-        doc_url = f"{const.DOCUPANDA_DOC_URL}{fileName}"
+    def post_document_subset(self, fileName):
+        doc_url = f"{const.DOCUPANDA_DOC_URL}"
+        print(doc_url)
         payload = {"document": {"file": {
             "contents": base64.b64encode(open(fileName, 'rb').read()).decode(),
             "filename": fileName
         }},
-            "pages": [2, 3, 4, 5, 6] #will set to pass on command line
+            "pages": [3,4,5,6,7,8] #will set to pass on command line
         }
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
             "X-API-Key": self.api_key
         }
+
         response = requests.post(doc_url, json=payload, headers=headers)
+        print(response.text)
         document_id = response.json()['documentId']
 
         return document_id
 
+    def post_document_all(self, fileName):
+        doc_url = f"{const.DOCUPANDA_DOC_URL}"
+        print (doc_url)
+        payload = {"document": {"file": {
+            "contents": base64.b64encode(open(fileName, 'rb').read()).decode(),
+            "filename": fileName
+        }}}
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-Key": self.api_key
+        }
+        response = requests.post(doc_url, json=payload, headers=headers)
+        print(response.json())
+        document_id = response.json()['documentId']
+
+        return document_id
     def get_full_ouptut(self, document_id):
         url = f"{const.DOCUPANDA_DOC_URL}{document_id}"
         headers = {
